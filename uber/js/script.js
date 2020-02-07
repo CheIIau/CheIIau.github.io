8eb3bb9c8dae72dcd9123aa867646a7c817ac316
CheIIau <rip.rulez123@gmail.com>
2020-02-07 13:09:16 +0300

    07.02.2020

--- uber/js/script.js
+++ uber/js/script.js
@@ -1,7 +1,8 @@
 window.addEventListener('DOMContentLoaded', () => {
     const menu = document.querySelector('.menu'),
-    menuItem = document.querySelectorAll('.menu__item'),
-    hamburger = document.querySelector('.hamburger');
+        menuItem = document.querySelectorAll('.menu__item'),
+        hamburger = document.querySelector('.hamburger'),
+        btn = document.querySelectorAll('.ntwrkbtn');
 
     hamburger.addEventListener('click', () => {
         hamburger.classList.toggle('hamburger_active');
@@ -12,6 +13,12 @@ window.addEventListener('DOMContentLoaded', () => {
         item.addEventListener('click', () => {
             hamburger.classList.toggle('hamburger_active');
             menu.classList.toggle('menu_active');
-        })
-    })
-})
+        });
+    });
+
+    btn.forEach(item => {
+        item.addEventListener('click', () => {
+            item.innerHTML = "Не жми, заебал";
+        });
+    });
+});
